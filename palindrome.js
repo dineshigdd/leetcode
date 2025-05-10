59c42e9fe40073a25ac47a2264aa9aed8a36b614
Dinesh <dineshigdd@gmail.com>
2025-05-10 08:40:31 -0700

    isPalindrome

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+var isPalindrome = function(x) {
+    //get the input
+    //put the input into an array
+    //compare first number of the interger to the last number
+    //if they are the same numebr , got the next number from both ends
+    //keep repeating this prcocess until middle number is read
+};
