8c2fc1148c78bc7f35a49512820bfb168d37cddf
Dinesh <dineshigdd@gmail.com>
2025-05-25 22:39:29 -0700

    pivotIndex.js

---
+++ pivotIndex.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    
+    /*read temp = nums[i]
+      index = i
+      get sum of left of num[i]
+      get the sum of right of num[i]
+      check if left_sum equals right_sum;
+      return index if it is equal
+      */
+
+
+ /* 
+    temp = nums[i];
+    index = i + 1;
+   
+        sum = temp;
+    For r = 0 To i
+        sum = sum + nums[r]
+    EndFor
+    
+    
+    */
+
+};
