a9cee6e947ae2e0132a249663d57ad0832050c66
Daminda Dinesh <dineshigdd@gmail.com>
2025-05-25 21:52:45 -0700

    Update romanToInt.js

--- romanToInt.js
+++ romanToInt.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    /*get the input and separate it into an array
+
+    print I if input is 1
+    print II if imput is 2
+    print III if input is 3
+
+    
+    If I is before V, subtract 1 from 5, else add 1 to 5
+    If I is before X, subtract 1 from 10, else add 1 to 10
+
+    If X is before L, subtract 10 from 50; else, add 10 to 50
+    If X is before C, subtract 10 from 100, else add 10 to 100
+
+    If C is before D, subtract 100 from 500; else, add 100 to 500
+    If C is before M, subtract 100 from 1000, else add 100 to 1000
+    
+    
+    */
     
 };
