6b91961562d8a2204e5968584ee6c7ce43d193df
Dinesh <dineshigdd@gmail.com>
2025-05-25 23:18:35 -0700

    add fucntion

--- romanToInt.js
+++ romanToInt.js
@@ -3,15 +3,41 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    /*get the input and separate it into an array
+    //get the input and separate it into an array
+    let IntegerArr = 0;
+    inputArr = s.split('');
+  
 
+/* 
     print I if input is 1
     print II if imput is 2
     print III if input is 3
 
-    
+*/
+inputArr.forEach((element, index , array ) => {
+       if( element == 'I' ){
+                IntegerArr += 1;
+   
+        }
+
+        if( array[ index ] === 'I' && array[index + 1 ] === 'V'){
+            
+        }
+
+/*
     If I is before V, subtract 1 from 5, else add 1 to 5
     If I is before X, subtract 1 from 10, else add 1 to 10
+*/
+
+
+
+
+});
+
+
+
+
+/*
 
     If X is before L, subtract 10 from 50; else, add 10 to 50
     If X is before C, subtract 10 from 100, else add 10 to 100
@@ -21,5 +47,7 @@ var romanToInt = function(s) {
     
     
     */
-    
+    return IntegerArr;
 };
+
+console.log(romanToInt('I'));
