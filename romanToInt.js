eebf4e136076eaba667e14a6d4ed3fc01e12ab4f
Dinesh <dineshigdd@gmail.com>
2025-06-01 08:45:43 -0700

    add code for sutractive principle

--- romanToInt.js
+++ romanToInt.js
@@ -49,8 +49,11 @@ var romanToInt = function(s) {
 
     console.log( IntOutput)
     let output = 0;
-    let addOutput = IntOutput[0];
-    IntOutput.forEach((element, index, array) => {
+   
+    //seperating digits that will be subjected additive principle
+     let addOutput = IntOutput[0];
+     let startIndex = 0;
+     IntOutput.forEach((element, index, array) => {
         /*  I can be placed before V (5) and X (10) to make 4 and 9. 
             X can be placed before L (50) and C (100) to make 40 and 90. 
             C can be placed before D (500) and M (1000) to make 400 and 900.*/
@@ -66,13 +69,28 @@ var romanToInt = function(s) {
             //     output =  array[ index + 1 ] - output
             // }
 
-
+            startIndex = index;
     })
 
 //to do
 // get the part of the IntOutput array that has the subtractive pronciple
-    let subOutput = IntOutput.slice() 
+
+    let subOutput = IntOutput[startIndex];  
     
+     IntOutput.forEach((element, index, array) => {
+        /*  I can be placed before V (5) and X (10) to make 4 and 9. 
+            X can be placed before L (50) and C (100) to make 40 and 90. 
+            C can be placed before D (500) and M (1000) to make 400 and 900.*/
+            
+            // console.log( element)
+            // console.log( output)
+            //subtractive principle          
+            if( array[ index + 1 ] > element ){
+                subOutput =  array[ index + 1 ] - subOutput
+            }
+
+          
+    })
 
 
     return output;
