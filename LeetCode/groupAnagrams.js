5ac421d81760362b24a6fde5f0944bd842be50f1
Dinesh <dineshigdd@gmail.com>
2025-09-11 19:48:18 -0700

    modify gropAnagram function

--- LeetCode/groupAnagrams.js
+++ LeetCode/groupAnagrams.js
@@ -4,29 +4,25 @@ class Solution {
      * @return {string[][]}
      * 
      */
-   
-
-    areAnagrams(str1, str2) {
-        if (str1.length !== str2.length) {
-            return false;
-        }
-
-        // if( str1 === '' || str2 === ''){
-        //     return false
-        // }
-        const sorted1 = str1.split('').sort().join('');
-        const sorted2 = str2.split('').sort().join('');
-        return sorted1 === sorted2;
-    }
-
     /*
     find the anagram
     put anagram to an subarray
     flag the anagram
 
-
-
     */
+
+    areAnagrams(str1, str2) { // find the anagram
+        if (str1.length !== str2.length) {
+            return false;
+        }
+  
+        const sorted1 = str1.split('').sort().join('');
+        const sorted2 = str2.split('').sort().join('');
+        return sorted1 === sorted2;
+    }
+
+
+   //grouping anagrams
     groupAnagrams(strs) {
        let output = [];
        
@@ -40,19 +36,31 @@ class Solution {
 
         for( let i = 0; i < strs.length  ; i++ ){    
             let subArray = [];
-            
-              
-                for( let j = i + 1; j < strs.length; j++ ){
-                    console.log( `compare ${ strs[i] } with ${ strs[j]}`)
+            console.log( i)
+            console.log( strs[i])
+            if( strs[i] !== '*'){
+                
+                subArray.push(strs[i] )
+
+                   for( let j = i + 1; j < strs.length; j++ ){
 
                     if( this.areAnagrams(strs[i], strs[j] ) ){ //find the angram
-                        subArray.push(strs[i] , strs[j])     //put into an sub array                   
+                 
+                        subArray.push( strs[j])     //put into an sub array                   
                         strs[j] = '*';          //flag the anagram
                     }
-                }
+                 }
+            }
+                 
+            
+              
+         
              
             // subArray.push(strs[i] )
-             output.push( subArray)
+            if( subArray.length !== 0 ){    
+                output.push( subArray)
+            }
+                
         }
        
         return output
@@ -63,8 +71,8 @@ class Solution {
 
 const solution = new Solution();
 // console.log( solution.groupAnagrams(["act","tap","cat","pat"]));
-// console.log( solution.groupAnagrams(["x"]));
-console.log( solution.groupAnagrams(["cat", "dog", "act", "stop"]));
+// // console.log( solution.groupAnagrams(["x"]));
+// console.log( solution.groupAnagrams(["cat", "dog", "act", "stop"]));
 // console.log(solution.areAnagrams('cat','cat'))
-// console.log( solution.groupAnagrams(["act","pots","tops","cat","stop","hat"]));
+console.log( solution.groupAnagrams(["act","pots","tops","cat","stop","hat"]));
 // Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
